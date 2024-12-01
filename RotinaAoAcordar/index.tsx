e7e68bcc10945ce051b79aa38ef29827b934cff8
Oliveira Santos <cammom131@gmail.com>
2024-11-30 22:12:11 -0300

    Primeiro commit

---
+++ RotinaAoAcordar/index.tsx
@@ -0,0 +1,18 @@
+"
+1. Acordar
+2. Se for 6 horas em ponto
+ 2.1 - Levantar
+ 2.3 - Ir ao banheiro
+ 2.3 - Escovar os dentes 
+ 2.4 - Tomar café
+ 2.5 - Ir para o trabalho
+
+3. Se  o horário for menor do que  6 horas
+ 3.1 - Continuar na cama 
+
+4. Se o horário for maior do que 6
+ 4.1 - Levantar
+ 4.2 - Ir ao banheiro
+ 4.3 - Escovar os dentes 
+ 4.4 - Ir para o trabalho
+"
